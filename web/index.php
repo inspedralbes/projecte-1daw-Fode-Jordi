17da35d2089685302021dda3764fbd63208873aa
a25fodkonsyl <a25fodkonsyl@inspedralbes.cat>
2026-05-02 15:54:29 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,12 +1,17 @@
 <?php include_once "header.php"; ?>
 
-aqui pondrfemos los botones para ir a las pcntl_async_signals
+<div class="text-center mt-5">
 
-<a class="btn btn-primary" href="crear_incidencia.php">Insertar incidencies</a>
-<a class="btn btn-primary" href="crear_incidencia.php">Veure estat incidencia</a>
+    <h1>Benvinguts a la nostra pàgina web !</h1>
 
-<a class="btn btn-primary" href="incidencies.php">Llistat d'incidencies</a>
+    <h4 class="mt-3"><br>Selecciona una de les tres opcions:</h4></br>
 
+    <div class="d-flex justify-content-center gap-3 mt-4">
+        <a href="crear_incidencia.php" class="btn btn-info btn-lg">Professor</a>
+        <a href="detall_incidencia.php" class="btn btn-success btn-lg">Tècnic</a>
+        <a href="incidencies.php" class="btn btn-danger btn-lg">Administrador</a>
+    </div>
 
+</div>
 
 <?php include_once "footer.php"; ?>
