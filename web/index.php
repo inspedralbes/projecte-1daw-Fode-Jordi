996cb7c21f84a835c28f25ab1c3ff0567cadd7f8
a25jormenlaz <a25jormenlaz@inspedralbes.cat>
2026-05-06 01:11:30 +0200

    Update welcome message and button labels

--- web/index.php
+++ web/index.php
@@ -1,17 +1,9 @@
 <?php include_once "header.php"; ?>
-
 <div class="text-center mt-5">
-
-    <h1>Benvinguts a la nostra pàgina web !</h1>
-
-    <h4 class="mt-3"><br>Selecciona una de les tres opcions:</h4></br>
-
+    <h1>Benvingut al gestor d'incidències</h1>
     <div class="d-flex justify-content-center gap-3 mt-4">
-        <a href="crear_incidencia.php" class="btn btn-info btn-lg">Professor</a>
-        <a href="detall_incidencia.php" class="btn btn-success btn-lg">Tècnic</a>
-        <a href="incidencies.php" class="btn btn-danger btn-lg">Administrador</a>
+        <a href="crear_incidencia.php" class="btn btn-info btn-lg">📝 Crear incidència</a>
+        <a href="incidencies.php" class="btn btn-danger btn-lg">📋 Veure llistat</a>
     </div>
-
 </div>
-
 <?php include_once "footer.php"; ?>
