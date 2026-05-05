64764bb78290b4d4bd825f8dc09d728d72500ac7
a25jormenlaz <a25jormenlaz@inspedralbes.cat>
2026-05-06 01:11:54 +0200

    Change language attribute and update title in header

--- web/header.php
+++ web/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="ca">
 <head>
     <meta charset="UTF-8">
-    <title> Gestor d'incidències informàtiques</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestor d'incidències</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-<div class="container">
-
+<div class="container my-4">
