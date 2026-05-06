61355ab2bdce4a73c5339804c3a7ae5528328a67
a25fodkonsyl <a25fodkonsyl@inspedralbes.cat>
2026-05-06 09:00:27 +0200

    Update header.php

--- web/header.php
+++ web/header.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html lang="ca">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestor d'incidències</title>
+    <title> Gestor d'incidències informàtiques</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-<div class="container my-4">
+<div class="container">
