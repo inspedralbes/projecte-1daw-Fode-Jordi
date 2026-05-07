1efae9624b2e4710016d07029a9c5897ad6cdd28
a25jormenlaz <a25jormenlaz@inspedralbes.cat>
2026-05-07 10:16:25 +0200

    Update footer.php

--- web/footer.php
+++ web/footer.php
@@ -1,6 +1,7 @@
 <footer class="px-2 py-2 fixed-bottom bg-dark">
-        <span class="text-muted">2026 By Fode & Jordi
-            
+        <span class="text-muted">
+            Grup 12 Fode - Jordi
+                1r DAW
         </span>
 </footer>
 
