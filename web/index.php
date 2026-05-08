52b693958ea4db9892269fb507d9e1f323c9b381
a25fodkonsyl <a25fodkonsyl@inspedralbes.cat>
2026-05-08 12:29:11 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,16 +2,16 @@
 
 <div class="text-center mt-5">
 
-    <h1>Benvinguts a la nostra pàgina web !</h1>
-
-    <h4 class="mt-3"><br>Selecciona una de les tres opcions:</h4></br>
+    <h1>Benvingut a la nostra pàgina web!</h1>
+    <h5 class="mt-3">Selecciona una de les tres opcions:</h5>
 
     <div class="d-flex justify-content-center gap-3 mt-4">
-        <a href="crear_incidencia.php" class="btn btn-info btn-lg">Professor</a>
-        <a href="detall_incidencia.php" class="btn btn-success btn-lg">Tècnic</a>
-        <a href="incidencies.php" class="btn btn-danger btn-lg">Administrador</a>
-    </div>
 
+            <a href="crear_incidencia.php" class="btn btn-info btn-lg">Professor</a>
+
+            <a href="tecnic.php" class="btn btn-success btn-lg">Tècnic</a>
+
+            <a href="admin.php" class="btn btn-danger btn-lg">Administrador</a>
 </div>
 
 <?php include_once "footer.php"; ?>
