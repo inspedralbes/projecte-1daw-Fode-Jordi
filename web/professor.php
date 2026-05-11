fe28999d991fbc15646beb97e8c37456acbd51be
a25jormenlaz <a25jormenlaz@inspedralbes.cat>
2026-05-11 08:42:40 +0200

    Add professor panel with navigation links

---
+++ web/professor.php
@@ -0,0 +1,18 @@
+<?php include_once "header.php"; ?>
+
+<div class="text-center mt-5">
+
+    <h2>Panel del Professor</h2>
+    <br>
+
+    <div class="d-flex justify-content-center gap-3">
+        <a href="crear_incidencia.php" class="btn btn-info btn-lg">Nova Incidència</a>
+        <a href="detall_incidencia.php" class="btn btn-warning btn-lg">Estat Incidència</a>
+    </div>
+
+    <br>
+    <a href="index.php" class="btn btn-secondary">Tornar a l'inici</a>
+
+</div>
+
+<?php include_once "footer.php"; ?>
