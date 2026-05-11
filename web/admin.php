068c9db09b046284dd4b2eb3998623bb71d6c34f
a25fodkonsyl <a25fodkonsyl@inspedralbes.cat>
2026-05-11 09:15:29 +0200

    Update admin.php

--- web/admin.php
+++ web/admin.php
@@ -6,8 +6,10 @@
     <br>
 
     <div class="d-flex justify-content-center gap-3">
-        <a href="incidencies.php" class="btn btn-dark btn-lg">Llistat d'incidències</a>
-        <a href="modificar_incidencia.php" class="btn btn-primary btn-lg">Modificar incidència</a>
+        <a href="incidencies.php" class="btn btn-danger btn-lg">Llistat d'incidències</a>
+        <a href="modificar_incidencia.php" class="btn btn-danger btn-lg">Modificar incidència</a>
+        <a href="informe_tecnics.php" class="btn btn-warning btn-lg">Informe tècnics</a>
+        <a href="informe_departaments.php" class="btn btn-warning btn-lg">Consum departaments</a>
     </div>
 
     <br>
