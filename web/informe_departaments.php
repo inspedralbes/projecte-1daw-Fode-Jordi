5efeff01bf9f61860561cc8b62da56f32d6b0a22
a25fodkonsyl <a25fodkonsyl@inspedralbes.cat>
2026-05-11 10:02:57 +0200

    Update informe_departaments.php

--- web/informe_departaments.php
+++ web/informe_departaments.php
@@ -1,19 +1,10 @@
 <?php
 include_once "connexio.php";
 
-$resultat = $conn->query("
-    SELECT 
-        D.nom AS nomDepartament,
-        COUNT(I.idIncidencia) AS numIncidencies,
-        (SELECT SUM(A.temps) 
-         FROM ACTUACIO A 
-         JOIN INCIDENCIA I2 ON A.incidencia = I2.idIncidencia 
-         WHERE I2.departament = D.idDepartament) AS tempsTotal
-    FROM DEPARTAMENT D
-    LEFT JOIN INCIDENCIA I ON I.departament = D.idDepartament
-    GROUP BY D.idDepartament
-    ORDER BY numIncidencies DESC
-");
+$sort = $_GET['sort'] ?? 'nomDepartament';
+$order = $_GET['order'] ?? 'ASC';
+
+$resultat = $conn->query("SELECT * FROM vista_consum_departaments ORDER BY $sort $order");
 ?>
 
 <?php include_once "header.php"; ?>
@@ -24,17 +15,29 @@ $resultat = $conn->query("
 <table class="table table-bordered">
     <thead>
         <tr>
-            <th>Departament</th>
-            <th>Nombre d'incidències</th>
-            <th>Temps total (minuts)</th>
+            <th>
+                Departament
+                <a href="?sort=nomDepartament&order=asc">↑</a>
+                <a href="?sort=nomDepartament&order=desc">↓</a>
+            </th>
+            <th>
+                Nombre d'incidències
+                <a href="?sort=nombreIncidencies&order=asc">↑</a>
+                <a href="?sort=nombreIncidencies&order=desc">↓</a>
+            </th>
+            <th>
+                Temps total (min)
+                <a href="?sort=tempsTotalDedicat&order=asc">↑</a>
+                <a href="?sort=tempsTotalDedicat&order=desc">↓</a>
+            </th>
         </tr>
     </thead>
     <tbody>
         <?php while ($fila = $resultat->fetch_assoc()): ?>
             <tr>
                 <td><?php echo $fila["nomDepartament"]; ?></td>
-                <td><?php echo $fila["numIncidencies"]; ?></td>
-                <td><?php echo $fila["tempsTotal"] ? $fila["tempsTotal"] : "0"; ?></td>
+                <td><?php echo $fila["nombreIncidencies"]; ?></td>
+                <td><?php echo $fila["tempsTotalDedicat"]; ?></td>
             </tr>
         <?php endwhile; ?>
     </tbody>
