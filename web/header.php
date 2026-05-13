dfcad89e2106dad2ee2d43925497357607b33b32
a25jormenlaz <a25jormenlaz@inspedralbes.cat>
2026-05-13 10:15:10 +0200

    Update header.php

--- web/header.php
+++ web/header.php
@@ -2,8 +2,16 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title> Gestor d'incidències informàtiques</title>
+    <title>Gestor d'incidències informàtiques</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body class="pb-5">
+
+<nav class="navbar bg-dark mb-4">
+    <div class="container">
+        <span class="navbar-brand text-white">Gestor d'Incidències</span>
+        <img src="https://www.institutpedralbes.cat/wp-content/uploads/2021/05/logo.jpg" height="50">
+    </div>
+</nav>
+
 <div class="container">
