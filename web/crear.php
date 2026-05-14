6647c9f7e0774c24329434691e3f555b56df6d73
a25fodkonsyl <a25fodkonsyl@inspedralbes.cat>
2026-05-14 09:38:48 +0200

    Update crear.php

--- web/crear.php
+++ web/crear.php
@@ -4,8 +4,8 @@ require_once 'connexio.php';
 
 /**
  * Funció que llegeix els paràmetres del formulari i crea una nova casa a la base de dades.
- * @param mixed $conn
- * @return void
+ * @param mixed 
+ * @return void+
  */
 function crear_casa($conn)
 {
@@ -16,19 +16,16 @@ function crear_casa($conn)
         return;
     }
 
-    // Preparar la consulta SQL per inserir una nova casa
     $sql = "INSERT INTO cases (name) VALUES (?)";
-    $stmt = $conn->prepare($sql);  //La variable $conn la tenim per haver inclòs el fitxer connexio.php
+    $stmt = $conn->prepare($sql); 
     $stmt->bind_param("s", $nom);
 
-    // Executar la consulta i comprovar si s'ha inserit correctament
     if ($stmt->execute()) {
         echo "<p class='info'>Casa creada amb èxit!</p>";
     } else {
         echo "<p class='error'>Error al crear la casa: " . htmlspecialchars($stmt->error) . "</p>";
     }
 
-    // Tancar la declaració i la connexió
     $stmt->close();
 
 }
@@ -49,11 +46,9 @@ function crear_casa($conn)
     <?php
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Si el formulari s'ha enviatc (mètode POST), cridem a la funció per crear la casa
         crear_casa($conn);
     } else {
-        //Mostrem el formulari per crear una nova casa
-        //Tanquem el php per poder escriure el codi HTML de forma més còmoda.
+        
         ?>
         <form method="POST" action="crear.php">
             <fieldset>
@@ -66,7 +61,6 @@ function crear_casa($conn)
 
 
         <?php
-        //Tanquem l'else
     }
     ?>
     <div id="menu">
